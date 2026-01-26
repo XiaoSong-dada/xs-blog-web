18b769c7ae695188f3b51fa41b05feb1c6ca6d54
xiaosong-dada <1935571817@qq.com>
2026-01-26 15:42:40 +0800

    #环境配置

--- config/env.ts
+++ config/env.ts
@@ -1,6 +1,9 @@
+const apiUrl = import.meta.env.VITE_BACKEND_API_URL;
+
 const config = {
     // Add your environment variables here
     TOKEN_KEY: 'token',
+    baseURL: apiUrl,
+    TIMEOUT: 20000
 }
-
 export default config
