8cb3e4a74d0420c31f999730b63149de969056e4
xiaosong-dada <1935571817@qq.com>
2026-01-27 15:09:09 +0800

    用户接口

--- src/api/user/user.ts
+++ src/api/user/user.ts
@@ -1,18 +1,9 @@
 import { requestHttp } from '@/utils/http';
 import type { ApiResponse, UserLoginData } from '@/types/main';
-import AuthService from '@/service/auth_service';
-
-export const login = async (data: UserLoginData): Promise<ApiResponse<{ token: string }>> =>  {
+export const login = async (data: UserLoginData): Promise<ApiResponse<{ token: string }>> => {
     const response = await requestHttp.request<{ token: string }>('/auth/login', {
         method: 'POST',
         data
     });
-
-    console.log(response);
-    
-    if (response.code === 200 && response.data?.token) {
-        AuthService.setToken(response.data.token);
-    }
-
     return response;
 }
