a283d5adbb5563adce03b079c4c834d296a39a70
xiaosong-dada <1935571817@qq.com>
2026-01-27 15:09:01 +0800

    用户业务

---
+++ src/service/auth.service.ts
@@ -0,0 +1,48 @@
+// /services/authService.ts
+import config from '@config/local.env';
+import useAuthStore from '../stores/auth';
+import Cookies from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
+import type { JwtCheckOptions } from '../types/main';
+import type { IUserInfo } from './../types/main';
+
+class AuthService {
+    static setToken(token: string) {
+        useAuthStore().setToken(token);
+        Cookies.set(config.TOKEN_KEY, token, { expires: 1, secure: true, sameSite: 'Strict' });
+    }
+
+    static getToken() {
+        return useAuthStore().token || Cookies.get(config.TOKEN_KEY);
+    }
+
+    static clearToken() {
+        useAuthStore().clearToken();
+        Cookies.remove(config.TOKEN_KEY);
+    }
+
+    static isTokenExpired(token: string, opt: JwtCheckOptions = {}): boolean {
+        const leewayMs = opt.leewayMs ?? 30_000;
+        const treatNoExpAsExpired = opt.treatNoExpAsExpired ?? false;
+        try {
+            const { exp } = jwtDecode<{ exp?: number }>(token);
+            if (!exp) return treatNoExpAsExpired;
+            return Date.now() >= exp * 1000 - leewayMs;
+        } catch {
+            return true;
+        }
+    }
+
+    static isUserTokenExpired(opt: JwtCheckOptions = {}):boolean {
+        const token = AuthService.getToken()
+        if (!token) return false;
+        return AuthService.isTokenExpired(token , opt)
+    }
+
+    static getUserInfo(): IUserInfo | null | undefined {
+        const token = AuthService.getToken();
+        return jwtDecode(token || '')
+    }
+}
+
+export { AuthService };
