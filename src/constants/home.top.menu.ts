78f1562c98743f99de15d16a141ede79b39b9256
xiaosong-dada <1935571817@qq.com>
2026-02-10 22:15:29 +0800

    #删除未开发功能

--- src/constants/home.top.menu.ts
+++ src/constants/home.top.menu.ts
@@ -1,6 +1,5 @@
 import type { HomeTopMenuItem } from '@/types/main'
 export const homeTopMenu:Array<HomeTopMenuItem> = [
     { key: 'home', label: '首页', route: '/' },
-    { key: 'article', label: '文章', route: '/article' },
-    { key: 'about', label: '关于', route: '/about' }
+    { key: 'article', label: '文章', route: '/article' }
 ]
